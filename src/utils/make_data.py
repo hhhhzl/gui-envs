a6608aeebac548ad68420318f0fcb2e91fe8d1ca
hectorrrrr <zhilinhe@usc.edu>
2025-04-03 19:28:43 -0400

    preposes

--- src/utils/make_data.py
+++ src/utils/make_data.py
@@ -1,9 +1,21 @@
+import os
+import json
 
-path = ''
+path = '/Users/zhilinhe/Desktop/hhhhzl/EduGetRicher/CMU/projects/GUI-VDILA/src/gui-envs/metadata/mapping'
+
+for filename in os.listdir(path):
+    if filename.endswith(".json"):
+        filepath = os.path.join(path, filename)
+        try:
+            with open(filepath, "r", encoding="utf-8") as file:
+                data = json.load(file)  # Load JSON content
+
+            file.close()
+        except json.JSONDecodeError as e:
+            print(f"Error decoding {filename}: {e}")
+        except Exception as e:
+            print(f"Error opening {filename}: {e}")
 
 
 def make_dataset():
     pass
-
-
-
